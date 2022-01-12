32e8e79017cd2c283a08ac570cb361fa83564520
anthonyguariglia <aguariglia197@gmail.com>
2022-01-12 09:45:05 -0500

    Add high-level files for web server

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from './shared/Layout'
+
+const App = () => (
+  <>
+    <Layout>
+    </Layout>
+  </>
+)
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/index.scss'
+import './css/spotifeed.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+const appJsx = (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(appJsx, document.getElementById('root'))
